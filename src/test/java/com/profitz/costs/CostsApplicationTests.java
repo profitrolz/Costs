3852c8d10ba977060838b93fe7212b5cca4f034f
Lexa <a.pisarenko9001@gmail.com>
2022-10-08 15:43:12 +0700

    Init commit

---
+++ src/test/java/com/profitz/costs/CostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profitz.costs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
